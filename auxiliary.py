74e09e6066f558e0d2be25fffec286fdfee9620b
末心 <59173630+Moxin1044@users.noreply.github.com>
2020-12-24 01:25:15 +0800

    Update LICENSE

--- auxiliary.py
+++ auxiliary.py
@@ -1,3 +1,17 @@
+"""
+Copyright (c) 2020-2021 Moxin
+   [Software Name] is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2.
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+   See the Mulan PSL v2 for more details.
+By：M0x1n Time：2020.12.23Updated
+Ver:1.2(Third edition) 1.2第三版
+https://bbs.moxinwangluo.cn/
+此脚本可以通过一些网站特征来进行CMS识别，当然，这个脚本也是开源的。
+This script can be used for CMS recognition based on some website features, and of course, this script is also open source.
+"""
 import os
 import re
 import sys
--- main.py
+++ main.py
@@ -15,6 +15,13 @@ import adminscan
 
 
 '''
+Copyright (c) 2020-2021 Moxin
+   [Software Name] is licensed under Mulan PSL v2.
+   You can use this software according to the terms and conditions of the Mulan PSL v2. 
+   You may obtain a copy of Mulan PSL v2 at:
+            http://license.coscl.org.cn/MulanPSL2 
+   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  
+   See the Mulan PSL v2 for more details.  
 By：M0x1n Time：2020.12.23Updated
 Ver:1.2(Third edition) 1.2第三版
 https://bbs.moxinwangluo.cn/
